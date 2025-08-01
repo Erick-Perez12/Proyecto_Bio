a37e80581e4452bbd26dcb93cfbe099433f0ab8c
AngelNifla <83251935+AngelNifla@users.noreply.github.com>
2025-08-01 01:51:18 -0500

    Agregando Mayusculas

---
+++ algorithms/mayusculas.py
@@ -0,0 +1,14 @@
+# mayusculas.py
+
+def transform_to_uppercase(sequence1, sequence2):
+    result = {
+        'sequence1_upper': sequence1.upper(),
+        'sequence2_upper': sequence2.upper()
+    }
+    return result
+def transform_to_lowecase(sequence1, sequence2):
+    result = {
+        'sequence1_upper': sequence1.lower(),
+        'sequence2_upper': sequence2.lower()
+    }
+    return result
